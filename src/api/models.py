5c4ba815034f789cdee007e65f0827f47633993f
Styoks <113370103+Styoks@users.noreply.github.com>
2024-08-07 19:21:19 +0000

    Starting project

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,150 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
+
+class MyRoles(enum.Enum):
+    customer = "Customer"
+    admin = "Admin"
+
+class StatusOrders(enum.Enum):
+    pending = "Pending"
+    completed = "Completed"
+    cancelled = "Cancelled"
+
+class PaymentMethods(enum.Enum):
+    credit_card = "Credit_card"
+    paypal = "Paypal"
+    cash = "Cash"
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    firstname = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.Enum(MyRoles), nullable=False, default = MyRoles.customer)
 
     def __repr__(self):
-        return f'<User {self.email}>'
-
+        return '<Users %r>' % self.id
+    
+    def new_user(self, username, password, email, name, firstname, role):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.name = name
+        self.firstname = firstname
+        self.role = role
+        db.session.add(self)
+        db.session.commit()
+        
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "name":self.name,
+            "firstname": self.firstname
+        }
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Enum(StatusOrders), nullable=False, default = StatusOrders.pending)
+    payment_method = db.Column(db.Enum(PaymentMethods), nullable=False, default = PaymentMethods.credit_card)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id, ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        return '<Order %r>' % self.id
+    
+    def new_order(self, status, payment_method, user_id):
+        self.status = status
+        self.payment_method = payment_method
+        self.user_id = user_id
+        db.session.add(self)
+        db.session.commit()
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "status": self.status,
+            "payment_method": self.payment_method,
+            "user_id":self.user_id,
+        }
+
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), primary_key=True, nullable=False)
+    description = db.Column(db.String(200), primary_key=True, nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Pizza %r>' % self.id
+        
+    def new_pizza(self, name, description, url):
+        self.name = name
+        self.description = description
+        self.url = url
+        db.session.add(self)
+        db.session.commit()
+        
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name' : self.name,
+            'description' : self.description,
+            'url': self.url
+        }
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    
+    def __repr__(self):
+        return '<Ingredient %r>' % self.id
+        
+    def new_ingredient(self, name):
+        self.name = name
+        db.session.add(self)
+        db.session.commit()
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name' : self.name,
+        }
+class PizzaIngredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    pizza_id = db.Column(db.Integer, db.ForeignKey(Pizza.id, ondelete="CASCADE"), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey(Ingredient.id, ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        return '<PizzaIngredient %r>' % self.id
+        
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'pizza_id': self.pizza_id,
+            'ingredient_id': self.ingredient_id
+        }
+
+class OrderItems(db.Model): 
+    id = db.Column(db.Integer, primary_key=True) 
+    order_id = db.Column(db.Integer, db.ForeignKey(Order.id, ondelete="CASCADE"), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey(Pizza.id, ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        return '<OrderItems %r>' % self.id
+    
+    def new_pizzas_order(self, order_id, pizza_id):
+        self.order_id = order_id
+        self.pizza_id = pizza_id
+        db.session.add(self)
+        db.session.commit()
+        
+    def serialize(self):
+        return {
+            'id': self.id,
+            'order_id': self.order_id,
+            'pizza_id': self.pizza_id
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,21 +2,302 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Pizza, Order, OrderItems, Ingredient, PizzaIngredient
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
-
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
+ 
+@api.route('/users', methods = ['GET'])
+def get_users(): 
+    users = User.query.all()
+    users_serialized = list(map(lambda item:item.serialize(), users))
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message" : "Nice!",
+        "data": users_serialized
+    }
+    if (users == []):
+        return jsonify({"msg": "Not users yet"}), 404
+    return jsonify(response_body), 200
+
+@api.route('/users/<int:user_id>', methods = ['GET'])
+def get_user(user_id): 
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": "User not found"}), 404
+        
+    user_info = User.query.filter_by(id=user_id).first().serialize()    
+    response_body = {
+        "message" : "Nice!",
+        "data": user_info
     }
 
     return jsonify(response_body), 200
+
+@api.route('/users/<int:user_id>', methods=['DELETE'])
+@jwt_required()
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        User.query.filter_by(id=user_id).delete()
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"msg": "User deleted"}), 200
+    else:
+        return jsonify({"msg": "User doesn't exist"}), 401
+    
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    users_query = User.query.filter_by(email=email).first()
+    if not users_query:
+        return jsonify({"msg": "Doesn't exist"}), 402
+    if password != users_query.password:
+        return jsonify({"msg": "Bad username or password"}), 401
+    print(users_query.role.value)
+
+    additional_claims = {
+        "user_id" : users_query.id,
+        "user_username" : users_query.username,
+        "user_role" : users_query.role.value
+    }
+    access_token = create_access_token(identity=users_query.id, additional_claims=additional_claims)
+    return jsonify(access_token=access_token), 200
+
+@api.route('/register', methods=['POST'])
+def register():
+    request_body = request.get_json()
+
+    if User.query.filter_by(email=request_body["email"]).first():
+        return jsonify({"msg": "Email already exists"}), 409
+   
+    user = User()
+    user.new_user(
+        email=request_body["email"],    
+        password=request_body["password"],
+        username=request_body["username"],
+        name = request_body["name"],
+        firstname = request_body["firstname"],
+        role = request_body["role"]
+    )
+    additional_claims = {
+        "user_id" : user.id,
+        "user_username" : user.username,
+        "role" : user.role.value
+    }
+
+    access_token = create_access_token(identity=request_body["email"], additional_claims=additional_claims)
+    return jsonify(access_token=access_token), 200
+
+@api.route('/pizzas', methods = ['GET'])
+@jwt_required()
+def get_pizzas(): 
+    pizzas = Pizza.query.all()
+    pizzas_serialized = list(map(lambda item:item.serialize(), pizzas))
+    response_body = {
+        "message" : "Nice pizzas!",
+        "data": pizzas_serialized
+    }
+    if (pizzas == []):
+        return jsonify({"msg": "Not pizzas yet"}), 404
+    return jsonify(response_body), 200
+
+@api.route('/pizzas', methods=['POST'])
+@jwt_required()
+def add_pizza():
+    request_body = request.get_json()
+
+    if Pizza.query.filter_by(id=request_body["id"]).first():
+        return jsonify({"msg": "Duplicated image"}), 409
+    jtw_data = get_jwt()
+    user_id = jtw_data["user_id"]
+    pizza = Pizza()
+    pizza.new_pizza(   
+        name= request_body["name"],
+        url=request_body["url"],
+        price = request_body["price"],
+        user_id = user_id
+    )
+
+    db.session.add(pizza)
+    db.session.commit()
+
+    return jsonify({"msg": "Pizza created", "pizza": pizza.serialize()}),201
+
+@api.route('/pizzas/<int:pizza_id>', methods = ['GET'])
+@jwt_required()
+def get_pizza(pizza_id): 
+    pizza = Pizza.query.get(pizza_id)
+    if pizza is None:
+        return jsonify({"msg": "Pizza not found"}), 404
+        
+    pizza_info = Pizza.query.filter_by(id=pizza_id).first().serialize()
+    response_body = {
+        "message" : "Nice pizza!",
+        "data": pizza_info
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/pizzas/<int:pizza_id>', methods=['DELETE'])
+@jwt_required()
+def delete_pizza(pizza_id):
+    pizza = Pizza.query.get(pizza_id)
+    if pizza:
+        Pizza.query.filter_by(id=pizza_id).delete()
+        db.session.delete(pizza)
+        db.session.commit()
+        return jsonify({"msg": "Pizza deleted"}), 200
+    else:
+        return jsonify({"msg": "Pizza doesn't exist"}),401
+
+@api.route('/orders', methods = ['GET'])
+@jwt_required()
+def get_orders(): 
+    orders = Order.query.all()
+    orders_serialized = list(map(lambda item:item.serialize(), orders))
+    response_body = {
+        "message" : "ok!",
+        "data": orders_serialized
+    }
+    if (get_orders == []):
+        return jsonify({"msg": "Not orders yet"}), 404
+    return jsonify(response_body), 200
+
+@api.route('/orders', methods=['POST'])
+@jwt_required()
+def new_order():
+    request_body = request.get_json()
+    if Order.query.filter_by(id=request_body["id"]).first():
+        return jsonify({"msg": "Duplicated order"}), 409
+    jtw_data = get_jwt()
+    user_id = jtw_data["user_id"]
+    order = Order(
+        id=request_body["id"],
+        status=request_body["status"],
+        payment_method=request_body["payment_method"],
+        user_id= user_id
+    )
+    db.session.add(order)
+    db.session.commit()
+
+    return jsonify({"msg": "Order created", "order": order.serialize()}), 201
+
+@api.route('/orders/<int:order_id>', methods = ['GET'])
+@jwt_required()
+def get_order(order_id): 
+    order = Order.query.get(order_id)
+    if order is None:
+        return jsonify({"msg": "Order not found"}), 404
+        
+    order_info = Order.query.filter_by(id=order_id).first().serialize()    
+    response_body = {
+        "message" : "ok!",
+        "data": order_info
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/orders/<int:order_id>', methods=['DELETE'])
+@jwt_required()
+def delete_order(order_id):
+    order = Order.query.get(order_id)
+    if order:
+        Order.query.filter_by(id=order_id).delete()
+        db.session.delete(order)
+        db.session.commit()
+        return jsonify({"msg": "Order deleted"}), 200
+    else:
+        return jsonify({"msg": "Order doesn't exist"}), 401
+    
+@api.route('/orderitems', methods=['POST'])
+@jwt_required()
+def new_order_item():
+    request_body = request.get_json()
+    if not request_body:
+        return jsonify({"msg": "Not found"}), 404
+
+    order_item_id = request_body.get("id")
+    order_id = request_body.get("order_id")
+    pizza_id = request_body.get("pizza_id")
+
+    if not all([order_item_id, order_id, pizza_id]):
+        return jsonify({"msg": "Missing fields"}), 400
+
+    if OrderItems.query.filter_by(id=order_item_id).first():
+        return jsonify({"msg": "Duplicated order item"}), 409
+
+    order_item = OrderItems()
+
+    order_item.new_pizzas_order(
+        id=order_item_id,
+        order_id=order_id,
+        pizza_id=pizza_id
+    )
+    db.session.add(order_item)
+    db.session.commit()
+
+    return jsonify({"msg": "Order item created", "order_item": order_item.serialize()}),
+    
+@api.route('/orderitems/<int:order_item_id>', methods=['GET'])
+@jwt_required()
+def get_order_item(order_item_id):
+    order_item = OrderItems.query.get(order_item_id)
+    if order_item is None:
+        return jsonify({"msg": "Order item not found"}), 404
+
+    order_item_info = order_item.serialize()
+    response_body = {
+        "message": "ok!",
+        "data": order_item_info
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/orderitems/<int:order_item_id>', methods=['DELETE'])
+@jwt_required()
+def delete_order_item(order_item_id):
+    order_item = OrderItems.query.get(order_item_id)
+    if order_item:
+        db.session.delete(order_item)
+        db.session.commit()
+        return jsonify({"msg": "Order item deleted"}), 200
+    else:
+        return jsonify({"msg": "Order item doesn't exist"}), 404
+
+@api.route('/ingredients', methods = ['GET'])
+@jwt_required()
+def get_ingredients(): 
+    ingredients = Ingredient.query.all()
+    ingredients_serialized = list(map(lambda item:item.serialize(), ingredients))
+    response_body = {
+        "message" : "ok!",
+        "data": ingredients_serialized
+    }
+    if (get_orders == []):
+        return jsonify({"msg": "Any ingredients yet"}), 404
+    return jsonify(response_body), 200
+
+@api.route('/ingredients', methods=['POST'])
+@jwt_required()
+def new_ingredient():
+    request_body = request.get_json()
+    if Ingredient.query.filter_by(id=request_body["id"]).first():
+        return jsonify({"msg": "Duplicated ingredient"}), 409
+    jtw_data = get_jwt()
+    user_role = jtw_data["user_role"]
+    if user_role == "admin":
+        ingredients = Ingredient()
+        ingredients.new_ingredient(
+            id=request_body["id"],
+            name=request_body["name"]
+        )
+        db.session.add(ingredients)
+        db.session.commit()
+
+    return jsonify({"msg": "Ingredient created", "ingredient": ingredients.serialize()}), 201
