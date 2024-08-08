5080f6d6b40da47533e69294eb41fe3b6d928c96
Styoks <113370103+Styoks@users.noreply.github.com>
2024-08-08 18:25:46 +0000

    Background and Navbar Colors

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img className="logo" src='https://res.cloudinary.com/didd1mjsp/image/upload/v1723140966/Untitled-2_n4lggj.png'/>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
